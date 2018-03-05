0f26bb8a5ff2e08a2a05062852c779807267efa7
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2018-03-05 17:44:37 +0100

    Basemap plot

--- basemap_raster/basemap_raster.py
+++ basemap_raster/basemap_raster.py
@@ -12,11 +12,11 @@
 import numpy as np
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
-from osgeo import gdal
+from osgeo import gdal, osr
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+# import os
 
 
-# color_map
 def color_map(plot_type="prob"):
     if (plot_type == "prob"):
         # Colormap
@@ -55,41 +55,48 @@ def color_map(plot_type="prob"):
 def convertXY(xy_source, inproj, outproj):
     # function to convert coordinates
 
-    shape = xy_source[0,:,:].shape
-    size = xy_source[0,:,:].size
+    shape = xy_source[0, :, :].shape
+    size = xy_source[0, :, :].size
 
     # the ct object takes and returns pairs of x,y, not 2d grids
     # so the the grid needs to be reshaped (flattened) and back.
     ct = osr.CoordinateTransformation(inproj, outproj)
     xy_target = np.array(ct.TransformPoints(xy_source.reshape(2, size).T))
 
-    xx = xy_target[:,0].reshape(shape)
-    yy = xy_target[:,1].reshape(shape)
+    xx = xy_target[:, 0].reshape(shape)
+    yy = xy_target[:, 1].reshape(shape)
 
     return xx, yy
 
 # Read the data and metadata
 ds = gdal.Open("areas_of_compromise.tif")
 
-data = ds.ReadAsArray()
+# Build overview
+ds.BuildOverviews("nearest", [2, 4, 8])
+band = ds.GetRasterBand(1)
+band.GetOverviewCount()
+
+# Get overview
+bandOv = band.GetOverview(1)
+data = bandOv.ReadAsArray()
 gt = ds.GetGeoTransform()
-proj = ds.GetProjection()
+proj = "+proj=aea +lat_1=20 +lat_2=-23 +lat_0=0 +lon_0=25 +x_0=0 +y_0=0 \
++datum=WGS84 +units=m +no_defs"
 
-xres = gt[1]
-yres = gt[5]
+xres = 4000
+yres = -4000
 
-# get the edge coordinates and add half the resolution 
+# get the edge coordinates and add half the resolution
 # to go to center coordinates
 xmin = gt[0] + xres * 0.5
-xmax = gt[0] + (xres * ds.RasterXSize) - xres * 0.5
-ymin = gt[3] + (yres * ds.RasterYSize) + yres * 0.5
+xmax = gt[0] + (xres * data.shape[1]) - xres * 0.5
+ymin = gt[3] + (yres * data.shape[0]) + yres * 0.5
 ymax = gt[3] - yres * 0.5
 
 ds = None
 
 # create a grid of xy coordinates in the original projection
-xy_source = np.mgrid[xmin:xmax+xres:xres, ymax+yres:ymin:yres]
-
+xy_source = np.mgrid[xmin:xmax + xres:xres, ymax + yres:ymin:yres]
 
 # Create the figure and basemap object
 fig = plt.figure()
@@ -102,7 +109,7 @@ m = Basemap(llcrnrlon=-20., llcrnrlat=-40., urcrnrlon=60., urcrnrlat=40.,
 # Create the projection objects for the convertion
 # original (Albers)
 inproj = osr.SpatialReference()
-inproj.ImportFromWkt(proj)
+inproj.ImportFromProj4(proj)
 
 # Get the target projection from the basemap object
 outproj = osr.SpatialReference()
@@ -112,7 +119,7 @@ outproj.ImportFromProj4(m.proj4string)
 xx, yy = convertXY(xy_source, inproj, outproj)
 
 # plot the data (first layer)
-im1 = m.pcolormesh(xx, yy, data[0,:,:].T, cmap=plt.cm.jet)
+im1 = m.pcolormesh(xx, yy, data.T, cmap=plt.cm.jet)
 
 # Draw country
 m.drawcoastlines()
@@ -123,9 +130,9 @@ m.drawparallels(np.arange(-40, 40, 20), labels=[1, 1, 0, 1])
 m.drawmeridians(np.arange(-20, 60, 20), labels=[1, 1, 0, 1])
 
 # Title
-ax.set_title("Area of compromise")
+ax.set_title("Areas of compromise")
 
 # Save plot
-plt.savefig('areas_of_compromise.png',dpi=75)
+plt.savefig('areas_of_compromise.png', dpi=300)
 
 # End
