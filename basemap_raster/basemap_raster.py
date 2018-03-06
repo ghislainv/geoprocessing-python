fa32c07805290675c6addc02ec843b5e7d4c6501
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2018-03-06 16:52:47 +0100

    Plot raster with basemap

--- basemap_raster/basemap_raster.py
+++ basemap_raster/basemap_raster.py
@@ -13,45 +13,22 @@ import numpy as np
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 from osgeo import gdal, osr
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-# import os
+from matplotlib.colors import ListedColormap
+
+# Color map
+# comp_dict = dict([[0,['#ffffff','not_suitable']],[1,['#006d2c','HL']],
+# [2,['#31a354','ML']],[3,['#bae4b3','LL']],
+# [4,['#2b8cbe','HM']],[5,['#a6bddb','MM']],
+# [6,['#ece7f2','LM']],[7,['#a50f15','HH']],
+# [8,['#de2d26','MH']],[9,['#fcae91','LH']],
+# [10,['#ff00ff','PA']]])
+# Set transparency for zero (#ffffff00)
+colors = ['#ffffff00', '#006d2c', '#31a354', '#bae4b3',
+          '#2b8cbe', '#a6bddb', '#ece7f2', '#a50f15',
+          '#de2d26', '#fcae91', '#ff00ff']
+color_map = ListedColormap(colors)
 
 
-def color_map(plot_type="prob"):
-    if (plot_type == "prob"):
-        # Colormap
-        colors = []
-        cmax = 255.0  # float for division
-        vmax = 65535.0  # float for division
-        colors.append((0, (0, 0, 0, 0)))  # transparent
-        colors.append((1 / vmax, (34 / cmax, 139 / cmax, 34 / cmax, 1)))
-        colors.append((45000 / vmax, (1, 165 / cmax, 0, 1)))  # orange
-        colors.append((55000 / vmax, (1, 0, 0, 1)))  # red
-        colors.append((1, (0, 0, 0, 1)))  # black
-        color_map = LinearSegmentedColormap.from_list(name="mycm",
-                                                      colors=colors,
-                                                      N=65535, gamma=1.0)
-    elif (plot_type == "fcc"):
-        # Colormap
-        colors = []
-        cmax = 255.0  # float for division
-        col_defor = (227, 26, 28, 255)
-        col_defor = tuple(np.array(col_defor) / cmax)
-        colors.append(col_defor)  # default is red
-        colors.append((51 / cmax, 160 / cmax, 44 / cmax, 1))  # forest green
-        colors.append((0, 0, 0, 0))  # transparent
-        color_map = ListedColormap(colors)
-    elif (plot_type == "forest"):
-        # Colormap
-        colors = []
-        cmax = 255.0  # float for division
-        colors.append((51 / cmax, 160 / cmax, 44 / cmax, 1))  # forest green
-        colors.append((0, 0, 0, 0))  # transparent
-        color_map = ListedColormap(colors)
-    return(color_map)
-
-
-# Coordinate conversion
 def convertXY(xy_source, inproj, outproj):
     # function to convert coordinates
 
@@ -74,15 +51,14 @@ gt = ds.GetGeoTransform()
 proj = ds.GetProjection()
 band = ds.GetRasterBand(1)
 
-# Build overview
+# Build overview at 5km
 if band.GetOverviewCount() == 0:
     ds.BuildOverviews("nearest", [5])
 
 # Get overview
 data = band.GetOverview(0).ReadAsArray()
 
-# get the edge coordinates and add half the resolution
-# to go to center coordinates
+# Get the coordinates of the center of the pixels
 xres = 5000
 yres = -5000
 xmin = gt[0] + xres * 0.5
@@ -90,45 +66,34 @@ xmax = gt[0] + (xres * data.shape[1]) - xres * 0.5
 ymin = gt[3] + (yres * data.shape[0]) - yres * 0.5
 ymax = gt[3] + yres * 0.5
 
+# Dereference raster
 ds = None
 
-# create a grid of xy coordinates in the original projection
-xy_source = np.mgrid[xmin:xmax + xres:xres, ymax - yres:ymin:yres]
+# Create a grid of xy coordinates in the original projection
+xy_source = np.mgrid[xmin:xmax + xres:xres, ymax:ymin + yres:yres]
 
 # Create the projection objects for the convertion
-inproj = osr.SpatialReference()
-inproj.ImportFromWkt(proj)
-# Get the target projection from the basemap object
-outproj = osr.SpatialReference()
-proj_merc = "+lon_0=20 +lat_ts=20.0 +R=6370997.0 +proj=merc \
-+x_0=-0.0 +units=m +y_0=4567371.38315"
-outproj.ImportFromProj4(proj_merc)
+in_proj = osr.SpatialReference()
+in_proj.ImportFromWkt(proj)
+out_proj = osr.SpatialReference()
+out_proj.ImportFromEPSG(4326)
+# Convert to latlong
+xx, yy = convertXY(xy_source, in_proj, out_proj)
 
-# Convert from source projection to basemap projection
-xx, yy = convertXY(xy_source, inproj, outproj)
+# Setup basemap
+plt.rcParams['hatch.linewidth'] = 0.001
+m = Basemap(resolution='l', area_thresh=100000., projection='cyl',
+            llcrnrlat=-28, llcrnrlon=-20, urcrnrlat=13, urcrnrlon=52)
+m.drawcoastlines(linewidth=0.1)
+m.drawcountries(linewidth=0.3, color='white')
+m.fillcontinents(color='darkgrey', lake_color='darkgrey',
+                 zorder=0)  # zorder=0 to paint over continents
 
-# Create the figure and basemap object
-fig = plt.figure()
-ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
-# setup mercator map projection.
-m = Basemap(llcrnrlon=-20., llcrnrlat=-40., urcrnrlon=60., urcrnrlat=40.,
-            resolution="l", projection="merc")
+# Plot the data
+xx_m, yy_m = m(xx, yy)  # Convert latlong coordinates to basemap proj
+im = m.pcolormesh(xx_m, yy_m, data.T, cmap=color_map)
 
-# plot the data
-im1 = m.pcolormesh(xx, yy, data.T, cmap=plt.cm.jet)
-
-# Draw country
-m.drawcoastlines()
-m.drawcountries()
-# draw parallels
-m.drawparallels(np.arange(-40, 40, 20), labels=[1, 1, 0, 1])
-# draw meridians
-m.drawmeridians(np.arange(-20, 60, 20), labels=[1, 1, 0, 1])
-
-# Title
-ax.set_title("Areas of compromise")
-
-# Save plot
-plt.savefig("areas_of_compromise.png", dpi=150)
+plt.title("Areas of compromise")
+plt.savefig("areas_of_compromise.png", dpi=300)
 
 # End
